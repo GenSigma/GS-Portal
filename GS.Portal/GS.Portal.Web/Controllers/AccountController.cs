8f6f87392d5b353ab97afed66a1ad7eb620deac7
sapan-gs <sapanp@gensigma.com>
2016-01-28 15:12:19 +0530

    Log Off redirects to Login page

--- GS.Portal/GS.Portal.Web/Controllers/AccountController.cs
+++ GS.Portal/GS.Portal.Web/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace GS.Portal.Web.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account"); 
         }
 
         //
