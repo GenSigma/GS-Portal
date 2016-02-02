c828109b173c5e87aa957dd9947f8433b36a58ea
sapan-gs <sapanp@gensigma.com>
2016-02-02 18:24:39 +0530

    UserRole.cs added

--- InitialScript.sql
+++ InitialScript.sql
@@ -1,8 +1,9 @@
 BEGIN
+	-- GS Portal DB creation
 	CREATE DATABASE [GS_Portal]
-
 	USE [GS_Portal]
 
+	-- Create Table [dbo].[AspNetRoles]
     CREATE TABLE [dbo].[AspNetRoles] (
         [Id] [nvarchar](128) NOT NULL,
         [Name] [nvarchar](256) NOT NULL,
@@ -10,6 +11,7 @@ BEGIN
     )
     CREATE UNIQUE INDEX [RoleNameIndex] ON [dbo].[AspNetRoles]([Name])
 
+	-- Create Table [dbo].[AspNetUserRoles]
     CREATE TABLE [dbo].[AspNetUserRoles] (
         [UserId] [nvarchar](128) NOT NULL,
         [RoleId] [nvarchar](128) NOT NULL,
@@ -18,6 +20,8 @@ BEGIN
     CREATE INDEX [IX_UserId] ON [dbo].[AspNetUserRoles]([UserId])
     CREATE INDEX [IX_RoleId] ON [dbo].[AspNetUserRoles]([RoleId])
 
+
+	-- Create Table [dbo].[AspNetUsers]
     CREATE TABLE [dbo].[AspNetUsers] (
         [Id] [nvarchar](128) NOT NULL,
         [Email] [nvarchar](256),
@@ -58,10 +62,11 @@ BEGIN
 	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-
+	-- Insert master user to [dbo].[AspNetUsers]
 	INSERT [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) 
-	VALUES (N'eca8fd88-c37f-4090-890e-953c6f024a70', N'sapan@gmail.com', 0, N'ADWh3iAkKc3cgPeJBJoCeNbXbnb4EQnWFpqOHBRV5uuH1ZdgZLoOIVMiY56uFiMYzg==', N'2ffda3c9-6c90-487d-aeb7-437c9cab351d', NULL, 0, 0, NULL, 1, 0, N'sapan@gmail.com')
+	VALUES (N'e952afc5-aa5c-426d-9b4d-a69158c53c79', N'master@gensigma.com', 0, N'ADeEwuqOKNHP8Q3AVU2Cnz1Tn2ESo5NgZaKc90RtVKSMqlETVOZ3VUmCKgGnri5J4A==', N'852bee0d-041e-423e-b367-ef152676e207', NULL, 0, 0, NULL, 1, 0, N'master@gensigma.com')
 
+	-- Create table [dbo].[AspNetUserClaims]
     CREATE TABLE [dbo].[AspNetUserClaims] (
         [Id] [int] NOT NULL IDENTITY,
         [UserId] [nvarchar](128) NOT NULL,
@@ -71,6 +76,7 @@ BEGIN
     )
     CREATE INDEX [IX_UserId] ON [dbo].[AspNetUserClaims]([UserId])
 
+	-- Create table [dbo].[AspNetUserLogins]
     CREATE TABLE [dbo].[AspNetUserLogins] (
         [LoginProvider] [nvarchar](128) NOT NULL,
         [ProviderKey] [nvarchar](128) NOT NULL,
@@ -83,5 +89,14 @@ BEGIN
     ALTER TABLE [dbo].[AspNetUserRoles] ADD CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
     ALTER TABLE [dbo].[AspNetUserClaims] ADD CONSTRAINT [FK_dbo.AspNetUserClaims_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
     ALTER TABLE [dbo].[AspNetUserLogins] ADD CONSTRAINT [FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+
+	-- Extending AspNetUser Table script
+	ALTER TABLE [dbo].[AspNetUsers] ADD [FirstName] [nvarchar](max) NOT NULL DEFAULT ''
+	ALTER TABLE [dbo].[AspNetUsers] ADD [MiddleName] [nvarchar](max) NOT NULL DEFAULT ''
+	ALTER TABLE [dbo].[AspNetUsers] ADD [LastName] [nvarchar](max) NOT NULL DEFAULT ''
+	ALTER TABLE [dbo].[AspNetUsers] ADD [EmployeeId] [nvarchar](max) NOT NULL DEFAULT ''
+	ALTER TABLE [dbo].[AspNetUsers] ADD [Role] [nvarchar](max) NOT NULL DEFAULT ''
+	ALTER TABLE [dbo].[AspNetUsers] ADD [EmpSeqNo] [int] NOT NULL DEFAULT 0
+	ALTER TABLE [dbo].[AspNetUsers] ADD [DateOfJoining] [datetime] NOT NULL DEFAULT '1900-01-01T00:00:00.000'
 END
 
