d4f4a1fe827d434eab3e521c2450dc70ab7df43d
sapan-gs <sapanp@gensigma.com>
2016-02-04 00:14:31 +0530

    Employee Area added

---
+++ GS.Portal/GS.Portal.Web/Areas/Employee/EmployeeAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace GS.Portal.Web.Areas.Employee
+{
+    public class EmployeeAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Employee";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Employee_default",
+                "Employee/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
